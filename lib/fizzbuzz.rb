d7bd7c95d278236b17833beeda3bc9b714504dd2
Dom <dom.mcdonnell@gmail.com>
2017-05-01 14:52:28 +0100

    passed fifth test returning fizzbuzz when passed 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-    'fizz'
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
   elsif number % 5 == 0
     'buzz'
-  elsif number % 3 == 0 && number % 5 == 0
-    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz'
   end
 end
