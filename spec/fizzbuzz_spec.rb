4e69a0f2b2d1685c5b19800b5667fb7278b7c049
Dom <dom.mcdonnell@gmail.com>
2017-05-01 14:45:31 +0100

    failed fifth test for fizzbuzz when passed 15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -23,3 +23,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(10)).to eq 'buzz'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect (fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
